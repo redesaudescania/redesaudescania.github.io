28d6eae91de6046dac4e7cfdef45579109a8dc9b
hugueds <hugo-pdf@hotmail.com>
2019-01-28 21:25:33 -0200

    Updates

---
+++ precache-manifest.f25c0719ce938fbb2ee720c6698b226a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e73f737126b9fc09e59",
+    "url": "/static/css/main.3d568906.chunk.css"
+  },
+  {
+    "revision": "3e73f737126b9fc09e59",
+    "url": "/static/js/main.3e73f737.chunk.js"
+  },
+  {
+    "revision": "1be557f6b5e48d549b89",
+    "url": "/static/js/1.1be557f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2722e6b22f65390043c0e1aea2f4401",
+    "url": "/static/media/logo.c2722e6b.svg"
+  },
+  {
+    "revision": "c08ac86b2944144f0c99aa6e8e5ea6c5",
+    "url": "/index.html"
+  }
+];
